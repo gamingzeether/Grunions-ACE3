3a01dc78106fb1b0f83f9f3d931489d8c9b71a0b
gamingzeether <80543808+gamingzeether@users.noreply.github.com>
2021-06-05 09:38:12 -0700

    add fyshing
    adds a throwable fish

---
+++ addons/fyshing/script_component.hpp
@@ -0,0 +1,16 @@
+#define COMPONENT fyshing
+#include "\z\ace\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_FYSHING
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_FYSHING
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_FYSHING
+#endif
+
+#include "\z\ace\addons\main\script_macros.hpp"
