9b44b1ec87dba4e608f98bf977f1760f3b6f547d
gamingzeether <80543808+gamingzeether@users.noreply.github.com>
2021-06-12 09:50:32 -0700

    reduce fysh time to 15 minutes

--- addons/fyshing/CfgAmmo.hpp
+++ addons/fyshing/CfgAmmo.hpp
@@ -4,7 +4,7 @@ class CfgAmmo {
     class Chemlight_base;
     
     class ACE_Fysh: Chemlight_base {
-        timeToLive = 43200;
+        timeToLive = 900;
         model = "\A3\animals_f\Fishes\tuna_F";
         effectsSmoke = "";
         EGVAR(advanced_throwing,torqueMagnitude) = 2;
