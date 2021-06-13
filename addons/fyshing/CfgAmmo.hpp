217515c1ae91319c428b7c250837874ed4185886
gamingzeether <80543808+gamingzeether@users.noreply.github.com>
2021-06-13 15:02:50 -0700

    add explosive fysh, fix fyshing

--- addons/fyshing/CfgAmmo.hpp
+++ addons/fyshing/CfgAmmo.hpp
@@ -2,12 +2,21 @@
 class CfgAmmo {
 
     class Chemlight_base;
-    
     class ACE_Fysh: Chemlight_base {
         timeToLive = 900;
-        model = "\A3\animals_f\Fishes\tuna_F";
+        model = "\A3\animals_f\Fishes\Tuna_F";
         effectsSmoke = "";
         EGVAR(advanced_throwing,torqueMagnitude) = 2;
+        //SmokeShellSoundHit1[] = {QPATHTOF(sounds\fyshing.ogg),2,1,50};
+        //grenadeFireSound[] = {"SmokeShellSoundHit1",0.5,};
+        explosionTime = 5;
+    };
+    
+    class GrenadeHand;
+    class ACE_FyshExplosive: GrenadeHand {
+        timeToLive = 900;
+        model = "\A3\animals_f\Fishes\Salema_porgy_F";
+        EGVAR(advanced_throwing,torqueMagnitude) = 4;
         //SmokeShellSoundHit1[] = {QPATHTOF(sounds\fyshing.ogg),2,1,50};
         //grenadeFireSound[] = {"SmokeShellSoundHit1",0.5,};
         explosionTime = 5;
--- addons/fyshing/CfgWeapons.hpp
+++ addons/fyshing/CfgWeapons.hpp
@@ -3,12 +3,15 @@ class CfgWeapons {
 
     class GrenadeLauncher;
     class Throw: GrenadeLauncher {
-        muzzles[] += {"ACE_FyshMuzzle"};
+        muzzles[] += {"ACE_Fysh_Muzzle", "ACE_Fysh_ExplosiveMuzzle"};
 
         class ThrowMuzzle;
         
-        class ACE_FyshMuzzle: ThrowMuzzle {
+        class ACE_Fysh_Muzzle: ThrowMuzzle {
             magazines[] = {"ACE_Fysh"};
+        };
+        class ACE_Fysh_ExplosiveMuzzle: ThrowMuzzle {
+            magazines[] = {"ACE_FyshExplosive"};
         };
     };
 };
