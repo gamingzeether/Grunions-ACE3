df0c5332224667dd9db903d5cd88b4f1a16c6c1c
gamingzeether <80543808+gamingzeether@users.noreply.github.com>
2021-06-13 15:02:50 -0700

    add explosive fysh, fix fyshing

--- addons/fyshing/CfgMagazines.hpp
+++ addons/fyshing/CfgMagazines.hpp
@@ -1,15 +1,24 @@
 class CfgMagazines {
 
     class SmokeShell;
-    
     class ACE_Fysh: SmokeShell {
         author = ECSTRING(common,ACETeam);
         ammo = "ACE_Fysh";
         displayName = CSTRING(Fysh_DisplayName);
-        displayNameShort = CSTRING(Fysh_DisplayNameShort);
+        displayNameShort = CSTRING(Fysh_DisplayName);
         descriptionShort = CSTRING(Fysh_DescriptionShort);
-        model = "\A3\animals_f\Fishes\tuna_F";
+        model = "\A3\animals_f\Fishes\Tuna_F";
         picture = QPATHTOF(UI\ace_fyshing_fysh_ca.paa);
         ACE_Attachable = "ACE_Fysh";
+    };
+    
+    class ACE_FyshExplosive: SmokeShell {
+        author = ECSTRING(common,ACETeam);
+        ammo = "ACE_FyshExplosive";
+        displayName = CSTRING(FyshExplosive_DisplayName);
+        displayNameShort = CSTRING(FyshExplosive_DisplayName);
+        descriptionShort = CSTRING(Fysh_DescriptionShort);
+        model = "\A3\animals_f\Fishes\Salema_porgy_F";
+        picture = QPATHTOF(UI\ace_fyshing_fysh_ca.paa);
     };
 };
