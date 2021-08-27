53768330c82c58b387530f813396df54e612dc4d
gamingzeether <80543808+gamingzeether@users.noreply.github.com>
2021-08-27 16:31:35 -0700

    Update CfgVehicles.hpp
    remove duplicate StaticWeapon

--- addons/cargo/CfgVehicles.hpp
+++ addons/cargo/CfgVehicles.hpp
@@ -320,6 +320,11 @@ class CfgVehicles {
     class StaticWeapon: LandVehicle {
         GVAR(size) = 2; // 1 = small, 2 = large
         GVAR(canLoad) = 1;
+        class VehicleTransport {
+            class Cargo {
+                canBeTransported = 1;
+            };
+        };
     };
 
     class StaticMortar;
@@ -814,13 +819,5 @@ class CfgVehicles {
     class Land_WaterBottle_01_stack_F: Items_base_F {
         GVAR(size) = 7;
         GVAR(canLoad) = 1;
-    };
-    
-    class StaticWeapon {
-        class VehicleTransport {
-            class Cargo {
-                canBeTransported = 1;
-            };
-        };
     };
 };
