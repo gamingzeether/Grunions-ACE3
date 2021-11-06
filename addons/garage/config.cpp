a1271d588023d206c847df76a9cc279c1c2ecb33
gamingzeether <gamingzeether123@gmail.com>
2021-11-06 16:15:28 -0700

    Update config.cpp
    remove repair requirement

--- addons/garage/config.cpp
+++ addons/garage/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"ace_common", "ace_repair"};
+        requiredAddons[] = {"ace_common"};
         author = ECSTRING(common,ACETeam);
         authors[] = {"GamingZeether"};
         url = ECSTRING(main,URL);
