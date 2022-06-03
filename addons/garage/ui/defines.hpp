769594db575a4b2b238c246cb5e7f2b995a50431
gamingzeether <80543808+gamingzeether@users.noreply.github.com>
2022-06-03 10:11:49 -0700

    garage - fix dialog not showing

--- addons/garage/ui/defines.hpp
+++ addons/garage/ui/defines.hpp
@@ -1,6 +1,9 @@
-#define pixelScale  0.25
-#define GRID_W (pixelW * pixelGridNoUIScale * pixelScale)
-#define GRID_H (pixelH * pixelGridNoUIScale * pixelScale)
+#define GUI_GRID_WAbs       ((safezoneW / safezoneH) min 1.2)
+#define GUI_GRID_HAbs       (GUI_GRID_WAbs / 1.2)
+#define GUI_GRID_W          (GUI_GRID_WAbs / 40)
+#define GUI_GRID_H          (GUI_GRID_HAbs / 25)
+#define GUI_GRID_X          (safezoneX)
+#define GUI_GRID_Y          (safezoneY + safezoneH - GUI_GRID_HAbs)
 
 #define IDD_ACE_GARAGE     1250001
 #define IDC_ACE_LISTBOX       0001
