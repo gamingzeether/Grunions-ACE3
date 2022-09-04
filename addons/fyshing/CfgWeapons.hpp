5f0fcab456e06392439013b1eda9afb312d3d638
gamingzeether <80543808+gamingzeether@users.noreply.github.com>
2022-09-03 18:25:07 -0700

    Fyshing - add fyshing cap

--- addons/fyshing/CfgWeapons.hpp
+++ addons/fyshing/CfgWeapons.hpp
@@ -21,4 +21,21 @@ class CfgWeapons {
         
         GVAR(ropeLength) = 50;
     };
+    
+    class H_Cap_red;
+    class ACE_H_Cap_Fysh: H_Cap_red {
+        author = ECSTRING(common,ACETeam);
+        displayName = CSTRING(cap_displayName);
+        picture = "\A3\characters_f\Data\UI\icon_H_Cap_tan_CA.paa";
+        hiddenSelectionsTextures[] = { QPATHTOF(data\capb_fysh_co.paa) };
+        class ItemInfo {
+            mass = 4;
+            uniformModel = "\A3\Characters_F\common\capb.p3d";
+            allowedSlots[] = { 801, 901, 701, 605 };
+            modelSides[] = { 6 };
+            scope = 0;
+            type = 605;
+            hiddenSelections[] = { "camo" };
+        };
+    };
 };
